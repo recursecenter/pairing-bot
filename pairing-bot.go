6eae938c430aaddd4c3f357511b56c080734f617
Maren Beam <maren@chro.bid>
2019-05-29 19:16:04 -0400

    migrated to firestore and messing with build
    in an effort to force myself to understand what /vendor is actually
    doing, I removed it and the accompanying go.mod and go.sum. At the same
    time, I rewrote a bunch of stuff such that the app now uses firestore
    rather than datastore. I'm sure these were all fantastic ideas. Please
    excuse me while I push 40 empty and/or failing commits in the next hour
    <3

--- pairing-bot.go
+++ pairing-bot.go
@@ -7,15 +7,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
-	"cloud.google.com/go/datastore"
+	"cloud.google.com/go/firestore"
 )
 
 const mcb int = 215391
 
 var err error
 
-type incomingHook struct {
+type incomingJSON struct {
 	BotEmail string `json:"bot_email"`
 	Data     string `json:"data"`
 	Token    string `json:"token"`
@@ -32,11 +33,104 @@ type botResponse struct {
 }
 
 type recurser struct {
-	SenderID       int
-	SenderFullName string
-	Subscribed     bool
+	ID   string `firestore:"id"`
+	Name string `firestore:"name"`
 }
 
+// Any incoming http request is handled here
+func handle(w http.ResponseWriter, r *http.Request) {
+
+	// 404 for anything other than /webhooks
+	if r.URL.Path != "/webhooks" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Look at the incoming webhook and slurp up the JSON
+	// Error if the JSON from Zulip istelf is bad
+	var userRequest incomingJSON
+	err = json.NewDecoder(r.Body).Decode(&userRequest)
+	if err != nil {
+		log.Println(err)
+		err = respond("Oof, Zulip may have just sent me invalid JSON.", w)
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	}
+
+	// validate RC's Zulip token
+	// if it doesn't validate, we don't know this person
+	// and thus don't give them anything
+	err = validateRequest(userRequest)
+	if err != nil {
+		log.Println(err)
+		http.NotFound(w, r)
+		return
+	}
+
+	// Act on a user request. This both parses and acts and responds
+	// Currently a bit of a catch-all. Candidate for breaking
+	// up later.
+	response, err := touchdb(userRequest)
+	if err != nil {
+		log.Println(err)
+		err = respond(response, w)
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Println("Bot attempted to respond but failed.")
+	}
+}
+
+func validateRequest(userRequest incomingJSON) error {
+	//validate the request
+	return nil
+}
+
+func touchdb(userRequest incomingJSON) (string, error) {
+	// if it's not Maren messaging the bot, just say uwu
+	if userRequest.Message.SenderID != mcb {
+		return `uwu`, nil
+	}
+
+	// Get set up to talk to the Firestore database
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, "pairing-bot-238901")
+	if err != nil {
+		return `error!`, err
+	}
+
+	// Get the data we need about the person making the request
+	recurser := recurser{
+		ID:   strconv.Itoa(userRequest.Message.SenderID),
+		Name: userRequest.Message.SenderFullName,
+	}
+	// key := firestore.NameKey("Recurser", "ZulipID", nil)
+
+	_, err = client.Collection("recursers").Doc(recurser.ID).Set(ctx, recurser, firestore.MergeAll)
+	if err != nil {
+		return `Something went sideways while writing to the Database`, err
+	}
+
+	response := fmt.Sprintf("Added %v to our database!", recurser.Name)
+	return response, nil
+}
+
+func respond(response string, w http.ResponseWriter) error {
+	err = json.NewEncoder(w).Encode(botResponse{response})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// It's alive! The app starts here.
 func main() {
 	http.HandleFunc("/", handle)
 
@@ -48,77 +142,4 @@ func main() {
 
 	log.Printf("Listening on port %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-}
-
-func handle(w http.ResponseWriter, r *http.Request) {
-	var userRequest incomingHook
-
-	// 404 for anything other than /webhooks
-	if r.URL.Path != "/webhooks" {
-		http.NotFound(w, r)
-		return
-	}
-
-	// Look at the incoming webhook and slurp up the JSON
-	// Error is the POST request from Zulip istelf is bad
-	err = json.NewDecoder(r.Body).Decode(&userRequest)
-	if err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-
-	// validate RC's Zulip instance token
-	err = validateRequest(userRequest)
-	if err != nil {
-		http.NotFound(w, r)
-		return
-	}
-
-	// Act on a user request. This both parses and acts and responds
-	// Currently a bit of a catch-all. Candidate for breaking
-	// up later.
-	response, err := touchdb(userRequest)
-	if err != nil {
-		err = json.NewEncoder(w).Encode(botResponse{`Something wrong with your command fren`})
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		return
-	}
-
-	err = json.NewEncoder(w).Encode(response)
-	if err != nil {
-		log.Println("Bot attempted to respond but failed.")
-	}
-}
-
-func validateRequest(userRequest incomingHook) error {
-	//validate the request
-	return nil
-}
-
-func touchdb(userRequest incomingHook) (botResponse, error) {
-	// if it's not Maren messaging the bot, just say uwu
-	if userRequest.Message.SenderID != mcb {
-		return botResponse{`uwu`}, nil
-	}
-
-	// Figure out what this does
-	ctx := context.Background()
-
-	datastoreClient, err := datastore.NewClient(ctx, "pairing-bot-238901")
-	if err != nil {
-		return botResponse{`error!`}, err
-	}
-
-	key := datastore.NameKey("Recurser", "ZulipID", nil)
-
-	zulipID := userRequest.Message.SenderID
-	fullName := userRequest.Message.SenderFullName
-	recurser := recurser{zulipID, fullName, false}
-	datastoreClient.Put(ctx, key, recurser)
-
-	response := botResponse{fmt.Sprintf("Added %v to our database!", fullName)}
-	return response, nil
 }

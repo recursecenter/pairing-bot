5a3ce1dd66b33263abf2f0f724a965d3c2b22474
Anushri Adhia <anushri.adhia@gmail.com>
2019-06-27 17:33:58 -0400

    added second API call to get people starting batch

--- message-batch/message-batch.go
+++ message-batch/message-batch.go
@@ -6,10 +6,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
 const key = ""
+const baseURL = "https://recurse.com/api/v1/"
 const layout = "2006-01-02"
 
 type Batch struct {
@@ -17,9 +19,54 @@ type Batch struct {
 	StartDate time.Time `json:"start_date"`
 }
 
+type Profile struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 // Start work on talking to the RC API
 func main() {
-	resp, err := http.Get("https://recurse.com/api/v1/batches?access_token=" + key)
+
+	respBody := makeAPICall("batches")
+
+	var batches []Batch
+	if err := json.Unmarshal(respBody, &batches); err != nil {
+		log.Fatal(err)
+	}
+
+	//Made the below to test
+	//sampleDate := "2019-05-20"
+	//currDate, _ := time.Parse(layout, sampleDate)
+
+	var currBatches []int
+	currDate := time.Now()
+	for _, batch := range batches {
+		if batch.StartDate == currDate {
+			currBatches = append(currBatches, batch.ID)
+		} else {
+			break
+		}
+	}
+
+	var profiles []Profile
+	for _, batchID := range currBatches {
+		var partialProfiles []Profile
+		respBody := makeAPICall("profiles", "batch_id="+fmt.Sprint(batchID))
+		if err := json.Unmarshal(respBody, &partialProfiles); err != nil {
+			log.Fatal(err)
+		}
+		profiles = append(profiles, partialProfiles...)
+	}
+
+}
+
+func makeAPICall(args ...string) []byte {
+
+	//TODO: not best way to concatenate strings; use strings.Builder instead
+	url := baseURL + args[0] + "?access_token=" + key + "&" + strings.Join(args[1:], "&")
+	resp, err := http.Get(url)
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,13 +77,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var batches []Batch
-	if err := json.Unmarshal(respBody, &batches); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(batches)
-
+	return respBody
 }
 
 func (batch *Batch) UnmarshalJSON(data []byte) error {

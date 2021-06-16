c9222db5d352e650b51cd43b2a304c6b6283bab5
Kathrin H <parsn1psoup@users.noreply.github.com>
2021-06-16 18:23:17 +0200

    Add unit tests for parseCmd

--- pairing-bot.go
+++ pairing-bot.go
@@ -11,7 +11,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -366,90 +365,6 @@ func main() {
 
 	log.Printf("Listening on port %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-}
-
-func parseCmd(cmdStr string) (string, []string, error) {
-	var err error
-	var cmdList = []string{
-		"subscribe",
-		"unsubscribe",
-		"help",
-		"schedule",
-		"skip",
-		"unskip",
-		"status"}
-
-	var daysList = []string{
-		"monday",
-		"tuesday",
-		"wednesday",
-		"thursday",
-		"friday",
-		"saturday",
-		"sunday"}
-
-	// convert the string to a slice
-	// after this, we have a value "cmd" of type []string
-	// where cmd[0] is the command and cmd[1:] are any arguments
-	space := regexp.MustCompile(`\s+`)
-	cmdStr = space.ReplaceAllString(cmdStr, ` `)
-	cmdStr = strings.TrimSpace(cmdStr)
-	cmdStr = strings.ToLower(cmdStr)
-	cmd := strings.Split(cmdStr, ` `)
-
-	// Big validation logic -- hellooo darkness my old frieeend
-	switch {
-	// if there's nothing in the command string srray
-	case len(cmd) == 0:
-		err = errors.New("the user-issued command was blank")
-		return "help", nil, err
-
-	// if there's a valid command and if there's no arguments
-	case contains(cmdList, cmd[0]) && len(cmd) == 1:
-		if cmd[0] == "schedule" || cmd[0] == "skip" || cmd[0] == "unskip" {
-			err = errors.New("the user issued a command without args, but it required args")
-			return "help", nil, err
-		}
-		return cmd[0], nil, err
-
-	// if there's a valid command and there's some arguments
-	case contains(cmdList, cmd[0]) && len(cmd) > 1:
-		switch {
-		case cmd[0] == "subscribe" || cmd[0] == "unsubscribe" || cmd[0] == "help" || cmd[0] == "status":
-			err = errors.New("the user issued a command with args, but it disallowed args")
-			return "help", nil, err
-		case cmd[0] == "skip" && (len(cmd) != 2 || cmd[1] != "tomorrow"):
-			err = errors.New("the user issued SKIP with malformed arguments")
-			return "help", nil, err
-		case cmd[0] == "unskip" && (len(cmd) != 2 || cmd[1] != "tomorrow"):
-			err = errors.New("the user issued UNSKIP with malformed arguments")
-			return "help", nil, err
-		case cmd[0] == "schedule":
-			for _, v := range cmd[1:] {
-				if contains(daysList, v) == false {
-					err = errors.New("the user issued SCHEDULE with malformed arguments")
-					return "help", nil, err
-				}
-			}
-			fallthrough
-		default:
-			return cmd[0], cmd[1:], err
-		}
-
-	// if there's not a valid command
-	default:
-		err = errors.New("the user-issued command wasn't valid")
-		return "help", nil, err
-	}
-}
-
-func contains(list []string, cmd string) bool {
-	for _, v := range list {
-		if v == cmd {
-			return true
-		}
-	}
-	return false
 }
 
 func nope(w http.ResponseWriter, r *http.Request) {
---
+++ parseCmd.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type parsingErr struct{ msg string }
+
+func (e parsingErr) Error() string {
+	return fmt.Sprintf("Error when parsing command: %s", e.msg)
+}
+
+func parseCmd(cmdStr string) (string, []string, error) {
+	var err error
+	var cmdList = []string{
+		"subscribe",
+		"unsubscribe",
+		"help",
+		"schedule",
+		"skip",
+		"unskip",
+		"status"}
+
+	var daysList = []string{
+		"monday",
+		"tuesday",
+		"wednesday",
+		"thursday",
+		"friday",
+		"saturday",
+		"sunday"}
+
+	// convert the string to a slice
+	// after this, we have a value "cmd" of type []string
+	// where cmd[0] is the command and cmd[1:] are any arguments
+	space := regexp.MustCompile(`\s+`)
+	cmdStr = space.ReplaceAllString(cmdStr, ` `)
+	cmdStr = strings.TrimSpace(cmdStr)
+	cmdStr = strings.ToLower(cmdStr)
+	cmd := strings.Split(cmdStr, ` `)
+
+	// Big validation logic -- hellooo darkness my old frieeend
+	switch {
+	// if there's nothing in the command string srray
+	case len(cmd) == 0:
+		err = errors.New("the user-issued command was blank")
+		return "help", nil, err
+
+	// if there's a valid command and if there's no arguments
+	case contains(cmdList, cmd[0]) && len(cmd) == 1:
+		if cmd[0] == "schedule" || cmd[0] == "skip" || cmd[0] == "unskip" {
+			err = &parsingErr{"the user issued a command without args, but it reqired args"}
+			return "help", nil, err
+		}
+		return cmd[0], nil, err
+
+	// if there's a valid command and there's some arguments
+	case contains(cmdList, cmd[0]) && len(cmd) > 1:
+		switch {
+		case cmd[0] == "subscribe" || cmd[0] == "unsubscribe" || cmd[0] == "help" || cmd[0] == "status":
+			err = &parsingErr{"the user issued a command with args, but it disallowed args"}
+			return "help", nil, err
+		case cmd[0] == "skip" && (len(cmd) != 2 || cmd[1] != "tomorrow"):
+			err = &parsingErr{"the user issued SKIP with malformed arguments"}
+			return "help", nil, err
+		case cmd[0] == "unskip" && (len(cmd) != 2 || cmd[1] != "tomorrow"):
+			err = &parsingErr{"the user issued UNSKIP with malformed arguments"}
+			return "help", nil, err
+		case cmd[0] == "schedule":
+			for _, v := range cmd[1:] {
+				if !contains(daysList, v) {
+					err = &parsingErr{"the user issued SCHEDULE with malformed arguments"}
+					return "help", nil, err
+				}
+			}
+			fallthrough
+		default:
+			return cmd[0], cmd[1:], err
+		}
+
+	// if there's not a valid command
+	default:
+		err = &parsingErr{"the user-issued command wasn't valid"}
+		return "help", nil, err
+	}
+}
+
+func contains(list []string, cmd string) bool {
+	for _, v := range list {
+		if v == cmd {
+			return true
+		}
+	}
+	return false
+}

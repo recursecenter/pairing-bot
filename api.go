5c200ac851adb0889bd437a33cadbbee06e566d8
Robert Xu <thecrxu@gmail.com>
2022-08-01 18:37:48 -0400

    add code to test API batching for profiles endpoint

--- api.go
+++ api.go
@@ -23,11 +23,43 @@ func (ra *RecurseAPI) userIsCurrentlyAtRC(accessToken string, email string) bool
 	return contains(emailsOfPeopleAtRC, email)
 }
 
-//The API endpoint this queries is updated at midnight on the last day (Friday) of a batch.
-//Make sure to only query this endpoint after it has been updated
+//The profile API endpoint is updated at midnight on the last day (Friday) of a batch.
 func (ra *RecurseAPI) getCurrentlyActiveEmails(accessToken string) []string {
 	var emailsOfPeopleAtRC []string
-	//TODO, batch the API call since the limit is 50 results per
+	offset := 0
+	limit := 25
+	apiHasMoreResults := true
+
+	for apiHasMoreResults {
+		emailsStartingFromOffset := ra.getCurrentlyActiveEmailsWithOffset(accessToken, offset, limit)
+
+		emailsOfPeopleAtRC = append(emailsOfPeopleAtRC, emailsStartingFromOffset...)
+
+		log.Println("The API returned this many profiles from the offset", len(emailsStartingFromOffset))
+
+		//The API limits respones to 50 total profiles. Keep querying the API until there are no more Recurser Profiles remaining
+		if len(emailsStartingFromOffset) == limit {
+			apiHasMoreResults = true
+			offset += limit
+
+			log.Println("We had more than limit results")
+		} else {
+			apiHasMoreResults = false
+			log.Println("We did not have more than the limit results")
+		}
+	}
+
+	log.Println("The API returned this many TOTAL profiles", len(emailsOfPeopleAtRC))
+
+	return emailsOfPeopleAtRC
+}
+
+/*
+	The RC API limits queries to the profiles endpoint to 50 results. However, there may be more than 50 people currently at RC.
+	The RC API takes in an "offset" query param that allows us to grab records beyond that limit of 50 results by performing multiple api calls.
+*/
+func (ra *RecurseAPI) getCurrentlyActiveEmailsWithOffset(accessToken string, offset int, limit int) []string {
+	var emailsOfPeopleAtRC []string
 
 	resp, err := http.Get(ra.rcAPIURL + "/profiles?scope=current&limit=50&role=recurser&access_token=" + accessToken)
 	if err != nil {

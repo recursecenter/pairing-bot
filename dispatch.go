2ae3a70e717862c75dae171a6c3e3017aa012655
Jeremy Kaplan <code@jdkaplan.dev>
2024-09-08 17:28:29 -0700

    refactor: Move dispatch returns into switch cases
    The only code after the switch statement is the return statement. Copy
    that return statement to each switch exit location.

--- dispatch.go
+++ dispatch.go
@@ -28,80 +28,85 @@ func (pl *PairingLogic) dispatch(ctx context.Context, cmd string, cmdArgs []stri
 	case "schedule":
 		if !isSubscribed {
 			response = notSubscribedMessage
-			break
+			return response, err
 		}
 
 		rec.Schedule = newSchedule(cmdArgs)
 
 		if err = pl.rdb.Set(ctx, userID, rec); err != nil {
 			response = writeErrorMessage
-			break
+			return response, err
 		}
 		response = "Awesome, your new schedule's been set! You can check it with `status`."
+		return response, err
 
 	case "subscribe":
 		if isSubscribed {
 			response = "You're already subscribed! Use `schedule` to set your schedule."
-			break
+			return response, err
 		}
 
 		rec.CurrentlyAtRC, err = pl.recurse.IsCurrentlyAtRC(ctx, userID)
 		if err != nil {
 			log.Printf("Could not read currently-at-RC data from database: %s", err)
 			response = writeErrorMessage
-			break
+			return response, err
 		}
 
 		if err = pl.rdb.Set(ctx, userID, rec); err != nil {
 			log.Printf("Could not update from database: %s", err)
 			response = writeErrorMessage
-			break
+			return response, err
 		}
 		response = subscribeMessage
+		return response, err
 
 	case "unsubscribe":
 		if !isSubscribed {
 			response = notSubscribedMessage
-			break
+			return response, err
 		}
 
 		if err := pl.rdb.Delete(ctx, userID); err != nil {
 			response = writeErrorMessage
-			break
+			return response, err
 		}
 		response = unsubscribeMessage
+		return response, err
 
 	case "skip":
 		if !isSubscribed {
 			response = notSubscribedMessage
-			break
+			return response, err
 		}
 
 		rec.IsSkippingTomorrow = true
 
 		if err := pl.rdb.Set(ctx, userID, rec); err != nil {
 			response = writeErrorMessage
-			break
+			return response, err
 		}
 		response = `Tomorrow: cancelled. I feel you. **I will not match you** for pairing tomorrow <3`
+		return response, err
 
 	case "unskip":
 		if !isSubscribed {
 			response = notSubscribedMessage
-			break
+			return response, err
 		}
 		rec.IsSkippingTomorrow = false
 
 		if err := pl.rdb.Set(ctx, userID, rec); err != nil {
 			response = writeErrorMessage
-			break
+			return response, err
 		}
 		response = "Tomorrow: uncancelled! Heckin *yes*! **I will match you** for pairing tomorrow :)"
+		return response, err
 
 	case "status":
 		if !isSubscribed {
 			response = notSubscribedMessage
-			break
+			return response, err
 		}
 		// this particular days list is for sorting and printing the
 		// schedule correctly, since it's stored in a map in all lowercase
@@ -147,6 +152,8 @@ func (pl *PairingLogic) dispatch(ctx context.Context, cmd string, cmdArgs []stri
 		}
 
 		response = fmt.Sprintf("* You're %v\n* You're scheduled for pairing on **%v**\n* **You're%vset to skip** pairing tomorrow", whoami, scheduleStr, skipStr)
+		return response, err
+
 	case "add-review":
 		reviewContent := cmdArgs[0]
 
@@ -161,10 +168,12 @@ func (pl *PairingLogic) dispatch(ctx context.Context, cmd string, cmdArgs []stri
 		if err != nil {
 			log.Println("Encountered an error when trying to save a review: ", err)
 			response = writeErrorMessage
-			break
+			return response, err
 		}
 
 		response = "Thank you for sharing your review with pairing bot!"
+		return response, err
+
 	case "get-reviews":
 		numReviews := 5
 
@@ -176,22 +185,30 @@ func (pl *PairingLogic) dispatch(ctx context.Context, cmd string, cmdArgs []stri
 		if err != nil {
 			log.Printf("Encountered an error when trying to fetch the last %v reviews: %v", numReviews, err)
 			response = readErrorMessage
-			break
+			return response, err
 		}
 
 		response = "Here are some reviews of pairing bot:\n"
 		for _, rev := range lastN {
 			response += "* \"" + rev.content + "\"!\n"
 		}
+		return response, err
+
 	case "cookie":
 		response = cookieClubMessage
+		return response, err
+
 	case "help":
 		response = helpMessage
+		return response, err
+
 	case "version":
 		response = pl.version
+		return response, err
+
 	default:
 		// this won't execute because all input has been sanitized
 		// by parseCmd() and all cases are handled explicitly above
+		return response, err
 	}
-	return response, err
 }
